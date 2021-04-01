6da82a2c440fc8b8f89d77d2e0640e146601b6de
crisyu <1021177281@qq.com>
2021-04-02 02:30:02 +0800

    正确爬取排名代码

--- university_ranking.py
+++ university_ranking.py
@@ -1,4 +1,4 @@
-#排名采用js格式，无法爬取
+# 排名采用js格式，无法爬取
 # 从网络上获取大学排名内容  getHTMLText()
 # 提取网页内容信息到合适的数据结构  fillUnivList()
 # 利用数据结构展示并输出结果  printUnivList()
@@ -19,30 +19,32 @@ def getHTMLText(url):  # 获取URL信息
         r.encoding = r.apparent_encoding
         return r.text
     except:
-        return ""
+        return "error"
 
 
 def fillUnivList(ulist, html):  # 将html页面放入list列表（ulist）
     soup = BeautifulSoup(html, "html.parser")
-    for td in soup.find('body').children:
-        if isinstance(th, bs4.element.Tag):
-            tds = th('td')
-            ulist.append([tds[0].string, tds[1].string], tds[2].string)
+    for tr in soup.find("tbody").children:
+        if isinstance(tr, bs4.element.Tag):
+            tds = tr.find_all('td')
+            aa = tds[1].find("a").text.strip()
+            ulist.append([tds[0].text.string(), aa,
+                          tds[4].text.string()])
 
 
 def printUnivList(ulist, num):  # 打印列表
-    print("{:^10}\t{:^6}\t{:^10}".format("排名", "学校名称", "城市"))
+    print("{:^10}\t{:^6}\t{:^10}".format("排名", "学校名称", "分数"))
     for i in range(num):
         u = ulist[i]
         print("{:^10}\t{:^6}\t{:^10}".format(u[0], u[1], u[2]))
 
 
 def main():
-    uinfo = []
-    url = 'https://www.shanghairanking.cn/rankings/bcur/202011'
+    url = 'https://www.shanghairanking.cn/_nuxt/static/1616049095/rankings/bcur/2020/payload.js'
+    ulist = []
     html = getHTMLText(url)
-    fillUnivList(uinfo, html)
-    printUnivList(uinfo, 20)
+    fillUnivList(ulist, html)
+    printUnivList(ulist, 20)
 
 
 main()
